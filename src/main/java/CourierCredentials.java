d30bfb8bf7f919f0184c52548e5df86a55420372
a.a.krasnova <crasalina13@gmail.com>
2025-01-19 18:51:17 +0300

    done sprint 7

---
+++ src/main/java/CourierCredentials.java
@@ -0,0 +1,14 @@
+public class CourierCredentials {
+
+    public String login;
+    public String password;
+
+    public CourierCredentials(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public static CourierCredentials from(Courier courier) {
+        return new CourierCredentials(courier.login, courier.password);
+    }
+}
